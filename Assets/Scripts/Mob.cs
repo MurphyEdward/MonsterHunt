b78948233fe467291b809ed8859e2214ab6ad10b
Elunded <demonschenel@gmail.com>
2023-03-28 15:52:35 +0300

    Додано першого моба
    Додав моба, та наказ йому рухатись за гравцем. Зробив кілька заготовок, для майбутньої механіки атаки

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform _player;
+
+    private bool _isFacingLeft;
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if((transform.position.x < _player.position.x && _isFacingLeft) || (transform.position.x > _player.position.x && !_isFacingLeft))
+        {
+            Flip();
+        }
+
+
+    }
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        
+        _rigidbody.velocity = new Vector2(_speed, _rigidbody.velocity.y);
+    }
+
+    private void Flip()
+    {
+            _speed *= -1;
+            _isFacingLeft = !_isFacingLeft;
+            transform.Rotate(0, 180, 0);
+    }
+}
